eab2fdf3dd3fdcc46173661a4bcc9094d21075b9
unknown <nguyen.michl@gmail.com>
2020-03-20 17:00:16 -0500

    Updates

---
+++ precache-manifest.55eccaadf2c024ec0faffe17e6abaf33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1266c8bd17b27d50e38d086649cefed",
+    "url": "/viet-type-assist/index.html"
+  },
+  {
+    "revision": "2bdf1762f661f1efbdbc",
+    "url": "/viet-type-assist/static/css/main.023da6ba.chunk.css"
+  },
+  {
+    "revision": "88d9a59c57a710daf6e2",
+    "url": "/viet-type-assist/static/js/2.e2d148bd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/viet-type-assist/static/js/2.e2d148bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bdf1762f661f1efbdbc",
+    "url": "/viet-type-assist/static/js/main.e51e29fd.chunk.js"
+  },
+  {
+    "revision": "5e76e0b3f15315095ad3",
+    "url": "/viet-type-assist/static/js/runtime-main.0f3033fc.js"
+  }
+]);
